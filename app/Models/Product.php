27b625312eb49abaed22cd995f22b0958a0c06e5
Adam Sabbah <Adam Sabbah>
2024-07-03 15:09:51 +0300

    updates

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -40,26 +40,36 @@ class Product extends Model
         $disk = "public"; // Use your desired disk
         $destination_path = "uploads/images"; // Your destination path
 
-        // If the images were erased
-        if ($value == null) {
-            Storage::disk($disk)->delete($this->{$attribute_name});
-            $this->attributes[$attribute_name] = null;
-        }
+        // Get the existing images from the database
+        $existingImages = $this->attributes[$attribute_name] ? json_decode($this->attributes[$attribute_name], true) : [];
 
-        // If the images were erased
-        if ($value == []) {
-            $this->attributes[$attribute_name] = null;
-        }
+        // Handle cleared images
+        $clearedImages = request()->input('clear_' . $attribute_name, []);
+        $existingImages = array_diff($existingImages, $clearedImages);
 
-        // If a file was uploaded
+        // If the value is an array, it means there are new files to upload
         if (is_array($value)) {
-            $images = [];
+            $newImages = [];
             foreach ($value as $file) {
-                $filename = $file->store($destination_path, $disk);
-                $images[] = $filename;
+                // Check if the file is a valid upload
+                if (is_a($file, 'Illuminate\Http\UploadedFile')) {
+                    // Store the file and get its path
+                    $filename = $file->store($destination_path, $disk);
+                    $newImages[] = $filename;
+                } else {
+                    // Add the existing file paths that are not cleared
+                    $newImages[] = $file;
+                }
             }
-            $this->attributes[$attribute_name] = json_encode($images);
+            // Merge new images with the remaining existing images
+            $allImages = array_merge($existingImages, $newImages);
+        } else {
+            // If no new files were uploaded, just use the existing images
+            $allImages = $existingImages;
         }
+
+        // Save the images array as JSON in the database
+        $this->attributes[$attribute_name] = json_encode($allImages);
     }
 
     // public function getImagesAttribute($value)
